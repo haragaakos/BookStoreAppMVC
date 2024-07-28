730c67831ba1d3ffa55bc44f447623aaa1d41e3c
Haraga Ákos <hakos@gmail.hu>
2024-07-28 20:05:34 +0200

    Category migration to Database; Category CRUD operations

--- BookStoreAppMVC/Program.cs
+++ BookStoreAppMVC/Program.cs
@@ -1,7 +1,13 @@
+global using BookStoreAppMVC.Data;
+global using BookStoreAppMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
